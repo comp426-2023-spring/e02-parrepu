a27247f5ddd1680afe9bdfccf56952f3b2bd5c82
Praneeth Arrepu <parrepu@live.unc.edu>
2023-04-28 14:39:48 -0400

    quick change to server file

--- server.js
+++ server.js
@@ -102,3 +102,33 @@ process.on('SIGINT', () => {
         }    
     })
 })
+// Everything after this was added from lecture (subject to change) -> Change endpoints to suit this assignment?
+// import {rps, rplsls} from "./lib/rpsls.js"; // Do I put the lib and bin folders into the public directory? | What's the path?
+
+app.get('/app/', (req, res, next) => {
+  res.status(200).send('200 OK');
+})
+
+app.get('/app/rps/play', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(rps());
+})
+  
+app.get('/app/rpsls/play', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(rpsls());
+})
+
+app.get('/app/rps/play/:shot', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(rps(req.params.shot)); 
+})
+
+app.get('/app/rpsls/play/:shot', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(rpsls(req.params.shot)); 
+})
+
+app.all('/app/*', (req, res, next) => {
+    res.status(404).send('404 NOT FOUND'); // Change following number to '404'
+})
